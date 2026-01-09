81c149d43793e64687af992055cfda93ebdef0c2
Geeky Lynx <73647896+geeky-lynx@users.noreply.github.com>
2026-01-09 21:15:16 +0100

    server: Create Users model

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Date, Float, String, Integer
 
 from server.env_loader import load_local_env
 
@@ -12,6 +14,51 @@ except Exception as error:
     print(f"{error}")
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = local_env["SQLALCHEMY_DATABASE_URI"]
+
+db = SQLAlchemy(app)
+
+
+
+class Users(db.Model):
+    __tablename__ = "users"
+    
+    id          = db.Column("id", Integer, primary_key = True)
+    email       = db.Column("email", String(50), unique = True, nullable = False)
+    password    = db.Column("password", String(500), nullable = False)
+    role        = db.Column("role", String(8), nullable = False) # USER | MANAGER | ADMIN
+    first_name  = db.Column("first_name", String(25))
+    last_name   = db.Column("last_name", String(25))
+    birth_date  = db.Column("birth_date", Date)
+    gender      = db.Column("gender", String(1))
+    country     = db.Coluumn("country", String(30)) # encrypted
+    street      = db.Column("street", String(50)) # encrypted
+    balance     = db.Column("balance", Float)
+    
+    def __init__(self,
+        email,
+        password,
+        role,
+        first_name,
+        last_name,
+        birth_date,
+        gender,
+        country,
+        street,
+        balance
+    ):
+        # __init__ body
+        self.email      = email
+        self.password   = password
+        self.role       = role
+        self.first_name = first_name
+        self.last_name  = last_name
+        self.birth_date = birth_date
+        self.gender     = gender
+        self.country    = country
+        self.street     = street
+        self.balance    = balance
 
 
 
