eb23e5b3f98f132c5a5cf2000fc1fa3ecf832a5c
Geeky Lynx <73647896+geeky-lynx@users.noreply.github.com>
2026-01-12 19:05:21 +0100

    Add /login and /register

--- server/app.py
+++ server/app.py
@@ -52,3 +52,80 @@ class User(db.Model):
 @app.route("/ping-reachable")
 def ping_reachable():
     return "<p>Server is reachable</p>"
+
+
+
+# TODO: Change responses
+@app.route("/login", methods = ["POST"])
+def user_login():
+    if "user" in session and session["user"] != "":
+        return "Already logged in"
+    
+    if request.method != "POST":
+        return "Invalid HTTP method"
+        
+    user_email = request.form["email"]
+    unhashed_password = request.form["password"]
+
+    is_email_empty = user_email is None or user_email == ""
+    is_password_empty = unhashed_password is None or unhashed_password == ""
+    if (is_email_empty or is_password_empty):
+        return "Invalid form request"
+        
+    hashed_password = bcrypt.hashpw(unhashed_password.encode("utf-8"), SALT)
+        
+    query = User.query.filter_by(email = user_email).first()
+    if query is None or query.password == hashed_password:
+        flash("Invalid email or password (or this account doesn\'t exist)", "error")
+        return "Invalid email or password (or this account doesn\'t exist)"
+    
+    session["user"] = user_email
+    return "Successfully logged in"
+
+
+
+@app.route("/register", methods = ["POST"])
+def user_register():
+    if "user" in session and session["user"] != "":
+        return "Logged in; must logout first"
+    
+    if request.method != "POST":
+        return "Invalid HTTP method"
+        
+    user_email = request.form["email"]
+    unhashed_password = request.form["password"]
+    repeated_password = request.form["password_repeated"]
+
+    is_email_empty = user_email is None or user_email == ""
+    is_password_empty = unhashed_password is None or unhashed_password == ""
+    is_repeated_empty = repeated_password is None or repeated_password == ""
+    if (is_email_empty or is_password_empty or is_repeated_empty):
+        return "Invalid form request"
+        
+    old_user = User.query.filter_by(email = user_email).first()
+    if old_user != None:
+        return "User with that email already exists"
+        
+    if unhashed_password != repeated_password:
+        flash("Passwords don\'t match", "error")
+        return "Passwords don\'t match"
+        
+    hashed_password = bcrypt.hashpw(unhashed_password.encode("utf-8"), SALT)
+    role = request.form["role"] if "role" in request.form else "USER"
+    first_name = request.form["first_name"]
+    last_name = request.form["last_name"]
+    birth_date = request.form["birth_date"]
+    gender = request.form["gender"]
+    country = request.form["country"]
+    street = request.form["street"]
+    
+    # TODO: Add DB query to add and handle errors
+    
+    return "TODO: Implement"
+
+
+
+if "__main__" == __name__:
+    with app.app_context():
+        db.create_all()
+    app.run(port = 8800, debug = True)
