f9131b8f51ba803a136d0f36c649ad8cf225f685
geeky-lynx <markomacura003@gmail.com>
2026-02-01 20:03:44 +0100

    server: Add 'IS_DEV' env parameter for debugging. improve code readibility

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from setup import app, db
+from setup import app, db, IS_DEV
 import routes # This adds routes to `setup.app` (Flask instance)
 
 
@@ -6,4 +6,4 @@ import routes # This adds routes to `setup.app` (Flask instance)
 if "__main__" == __name__:
     with app.app_context():
         db.create_all()
-    app.run(port = 8800, debug = True)
+    app.run(port = 8800, debug = IS_DEV)
--- server/setup.py
+++ server/setup.py
@@ -1,4 +1,4 @@
-from os import getenv
+from os import environ, getenv
 from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
@@ -16,28 +16,41 @@ except Exception as error:
     exit(2)
 print("Local .env is loaded")
 
-app = Flask(__name__)
-CORS(app)
 
-SECRET_KEY = getenv("SECRET_KEY")
-DB_URI = getenv("SQLALCHEMY_DATABASE_URI")
+
+_IS_DEV_STR = getenv("IS_DEV")
+_SECRET_KEY = getenv("SECRET_KEY")
+_DB_URI = getenv("SQLALCHEMY_DATABASE_URI")
 
 # Stop the program if there are no config parameters
-if SECRET_KEY is None:
+if _SECRET_KEY is None:
     print("SECRET_KEY == None! Can\'t proceed with running Flask instance")
     exit(2)
 
-if DB_URI is None:
+if _DB_URI is None:
     print("DB_URI == None! Can\'t proceed with running Flask instance")
     exit(2)
 
 
+
+IS_DEV = True if _IS_DEV_STR is str and _IS_DEV_STR.lower() in ["1", "true"] else False
+SECRET_KEY: str = _SECRET_KEY
+DB_URI: str = _DB_URI
+
+
+
+app = Flask(__name__)
+CORS(app)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_URI
 app.secret_key = SECRET_KEY
+
+
 
 class DbBase(DeclarativeBase):
     pass
 
+
+
 db = SQLAlchemy(app, model_class = DbBase)
 SALT = bcrypt.gensalt()
-
